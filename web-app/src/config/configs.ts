c8206a535cf1d397d611170e6cae0d4f113018c0
Sachintha Dissanayake <124299595+SachinthaNimesh@users.noreply.github.com>
2025-07-18 17:59:33 +0530

    API URL changed to backend service

--- web-app/src/config/configs.ts
+++ web-app/src/config/configs.ts
@@ -1,4 +1,4 @@
-export const API_URL = "/choreo-apis/employee-mgmt-system/server-docker/v1";
+export const API_URL = "/choreo-apis/employee-mgmt-system/backend/v1";
 // export const API_URL =
 // "https://87e89eab-95e5-4c0f-8192-7ee0196e1581-dev.e1-us-east-azure.choreoapis.dev/employee-mgmt-system/server-docker/v1.0";
 
